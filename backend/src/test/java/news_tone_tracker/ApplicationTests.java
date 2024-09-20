06fe29e9c1d54bd4ebb82a9aca2fc9ca9432285f
Bohdan <bohdan.sosunovych@gmail.com>
2024-09-20 11:00:09 +0300

    first commit

---
+++ backend/src/test/java/news_tone_tracker/ApplicationTests.java
@@ -0,0 +1,13 @@
+package news_tone_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
